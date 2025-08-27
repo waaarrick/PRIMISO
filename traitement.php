3a3a5b37ee8cde042eb55253c11c57cfffd455a4
waaarrick <165796207+waaarrick@users.noreply.github.com>
2025-08-27 20:52:40 +0200

    Add files via upload

---
+++ traitement.php
@@ -0,0 +1,47 @@
+<?php
+// Inclure PHPMailer
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+$prenom = $_POST['prenom'];
+$nom = $_POST['nom'];
+$email = $_POST['email'];
+$sujet = $_POST['sujet'];
+$message=$_POST['message'];
+
+$message = "Nom : ".$nom."\n"."Email : ".$email."\n"."message : ".$message;
+
+try {
+    //Server settings
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'mansoibou.warrick@gmail.com';                     //SMTP username
+    $mail->Password   = 'vxrm gsdm uiis oyan';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('from@example.com', 'primiso');
+    $mail->addAddress('contact@primiso.fr');     //Add a recipient
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = $message;
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+?>
