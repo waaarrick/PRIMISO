5acc67c0d91b4da70cd06d76b64f707f3b77445b
Mansoibou Warrick <mansoibou.warrick@gmail.com>
2026-09-24 16:26:04 +0200

    fix connexion SMTP port 587

--- primiso-backend/server.js
+++ primiso-backend/server.js
@@ -154,7 +154,9 @@ app.post('/contact', uploadSansFichier.none(), async (req, res) => {
 
   try {
     const transporteur = nodemailer.createTransport({
-      service: 'gmail',
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASSWORD,
